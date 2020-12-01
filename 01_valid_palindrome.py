c6fcab49f4b5495ba9e49987e9d18786e8abb51e
DHyun <castorhyun@gmail.com>
2020-12-01 18:24:45 +0900

    add 02_reverse string

--- 01_valid_palindrome.py
+++ 01_valid_palindrome.py
@@ -1,6 +1,8 @@
+import re
+
 class Solution:
 
-    def isPalindrome(self, s: str) -> bool:
+    def isPalindrome_pop(self, s: str) -> bool:
         # s : alphabet and numeric only
 
         check_str = []
@@ -19,6 +21,19 @@ class Solution:
 
         return True
 
+    def isPalindrome_slicing(self, s: str) -> bool:
+
+        # convert to lower char
+        s = s.lower()
+
+        # 정규식 : a-z0-9 : 소문자,숫자 형태 아닌 경우 '' 변환
+        s = re.sub('[^a-z0-9]', '', s)
+
+        # 역순으로 정렬
+        s_rev = s[::-1]
+
+        return s == s_rev
+
 
 if __name__ == "__main__":
     T = Solution()
@@ -26,6 +41,12 @@ if __name__ == "__main__":
     str1 = 'is a palidrome?'
     str2 = 'superman am rep us'
 
-    print('str1 is :', T.isPalindrome(str1))
-    print('str2 is :', T.isPalindrome(str2))
+    print("pop fn test")
+    print('str1 is :', T.isPalindrome_pop(str1))
+    print('str2 is :', T.isPalindrome_pop(str2))
+
+    print("slicing fn test")
+    print('str1 is :', T.isPalindrome_slicing(str1))
+    print('str2 is :', T.isPalindrome_slicing(str2))
+
 
---
+++ 02_reverse_string.py
@@ -0,0 +1,38 @@
+
+
+class Solution:
+
+    def reverseSting_2pointer(self, s):
+        left, right = 0, len(s) - 1
+
+        print("Input Str:", s)
+
+        list_str = list(s)
+        print("list_str:", list_str)
+
+        while left < right:
+            # switch left & right
+            list_str[left] , list_str[right] = list_str[right], list_str[left]
+
+            left += 1
+            right -= 1
+
+        print("reversed Str:", list_str)
+
+        return "".join(list_str)
+
+    def reverseString_func(self, s):
+
+        # reverse 함수는 list 에서만 사용가
+        # list(s).reverse()
+        return s[::-1]
+
+if __name__ == "__main__":
+    T = Solution()
+
+    print('reverseSting_2pointer : ', T.reverseSting_2pointer('Apple is delicious'))
+
+    S = 'Apple is delicious'
+
+    print("reverseString_func : ", T.reverseString_func(S))
+
